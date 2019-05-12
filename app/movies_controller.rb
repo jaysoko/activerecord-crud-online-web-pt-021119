0d5faa6030ff26a735751d54b84f321d748acb1e
Jay Sokolowski <jaysoko@gmail.com>
2019-05-12 20:00:29 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -30,7 +30,6 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: "1990"}
   Movie.create do |m|
     Movie.new(args)
     end
-  end
 end
 
 def can_get_the_first_item_in_the_database
