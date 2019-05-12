a631b43ca53bc3452cf75f9c773d3eadfe63f8b7
Jay Sokolowski <jaysoko@gmail.com>
2019-05-12 20:02:55 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -34,11 +34,11 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: "1990"}
 end
 
 def can_get_the_first_item_in_the_database
-  Movies.first
+  Movie.first
 end
 
 def can_get_the_last_item_in_the_database
-  Movies.last
+  Movie.last
 end
 
 def can_get_size_of_the_database
